9379712994d6ed5a248cfecf9c1363c8657c8bd5
Vinicius <viniholtman28@gmail.com>
2022-07-10 01:22:01 -0300

    Criado metricas para usuarios

--- web/pages/dashboard.tsx
+++ web/pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from 'react'
 import { AuthContext } from '../contexts/AuthContext'
+import { useCan } from '../hooks/useCan'
 import { setupAPIClient } from '../services/api'
 import { api } from '../services/apiClient'
 
@@ -8,6 +9,10 @@ import { withSSRAuth } from "../utils/withSSRAuth"
 export function Dashboard() {
   const { user } = useContext(AuthContext)
 
+  const userCanSeeMetrics = useCan({
+    permissions: ['metrics.list']
+  })
+
   useEffect(() => {
     api.get('/me')
     .then(response => console.log(response))
@@ -15,7 +20,11 @@ export function Dashboard() {
   })
 
   return (
-    <h1>Hello world {user?.email}</h1>
+    <>
+      <h1>Hello world {user?.email}</h1>
+
+      { userCanSeeMetrics && <h2>Metricas</h2> }
+    </>
   )
 }
 
