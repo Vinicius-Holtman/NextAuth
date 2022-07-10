0ce08994f68316597c87478eb5d3fa173c5ef7d7
Vinicius <viniholtman28@gmail.com>
2022-07-09 23:48:36 -0300

    Validaty authenticate server

--- web/pages/dashboard.tsx
+++ web/pages/dashboard.tsx
@@ -1,6 +1,9 @@
 import { useContext, useEffect } from 'react'
 import { AuthContext } from '../contexts/AuthContext'
-import { api } from '../services/api'
+import { setupAPIClient } from '../services/api'
+import { api } from '../services/apiClient'
+
+import { withSSRAuth } from "../utils/withSSRAuth"
 
 export function Dashboard() {
   const { user } = useContext(AuthContext)
@@ -15,3 +18,13 @@ export function Dashboard() {
     <h1>Hello world {user?.email}</h1>
   )
 }
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx)
+  const reponse = await apiClient.get('/me')
+
+  console.log(reponse)
+  return {
+    props: {}
+  }
+})
